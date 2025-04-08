0d88f42f29ea4ff712f657c19f35baceeaca2fd8
Christian <christianespinozacel@westernwyoming.edu>
2025-04-08 06:13:00 +0000

    Created a file to update changes of the new model - markII, finally created the proper calling with the dictionary

--- new_markII.py
+++ new_markII.py
@@ -1,15 +1,11 @@
 #PLEASE READ BEFORE RUNNING#
 #Be sure to provide three arguments when calling from the terminal
 #This will be the name of the program ("py programName"), the file destination, and the folder/directory to use
-#python testing_newModel.py all_info_four filesystem/machine1-data/
+#python new_markII.py all_info_four filesystem/machine1-data/
 import pandas as pandas
 import sys
 import glob 
-#Glob simply means global.
 
-
-#Function checks for the arguments given in the terminal, first arg. must be the program name,second the destination
-#and the third being the directory the program is under. 
 def checking_args():
 
     if len(sys.argv) > 3:
@@ -21,27 +17,32 @@ def checking_args():
     else:
         print("Perfect")
 
-#sys.argv[2] is the file path containing the excel file(s)
-#This function extracts all the xlsx files from the directory/folder.
 def extracting_excel():
     accumulative_frame = pandas.DataFrame()
-    #Checks the last argument in the terminal statement, gets the directory name and finds all xlsx files
-    for file in glob.iglob(f'{sys.argv[2]}**/*.xlsx', recursive=True):
-        #Create an instance from the ExcelFile class to check for multiple sheets
-        excel = pandas.ExcelFile(file)
-        #A variable that associates itself with the number of sheets within an excel file.
+   
+    dir = sys.argv[2] 
+    for i in glob.iglob('**/*.xlsx', root_dir=dir, recursive=True):
+        path = dir + i
+        print("this is i")
+        print(path)
+    
+    for i in glob.iglob('**/*.xlsx', root_dir=dir, recursive=True):
+        path = dir + i
+        excel = pandas.ExcelFile(path)
         sheet_names = excel.sheet_names
-
         if len(sheet_names) > 1:
-            print('Contains Multiple Sheets')
             for eachSheet in sheet_names:
                 print(eachSheet)
+                temp_frame = pandas.read_excel(eachSheet)
+                print(temp_frame)
+                accumulative_frame = pandas.concat([temp_frame, accumulative_frame])
         else:
-            temp_frame = pandas.read_excel(file)
+            temp_frame = pandas.read_excel(path)
             print(temp_frame)
             print(accumulative_frame)
             accumulative_frame = pandas.concat([temp_frame, accumulative_frame])
             print('Running Through This')
+
     print('passes this section')
     accumulative_frame.to_csv("all_info_four")
     
--- playground.py
+++ playground.py
@@ -2,12 +2,25 @@ import pandas as pandas
 import glob
 import sys
 
+accumulative_frame = pandas.DataFrame()
 dir = sys.argv[2] 
 for i in glob.iglob('**/*.xlsx', root_dir=dir, recursive=True):
         path = dir + i
         print("this is i")
         print(path)
 
+        temp_frame = pandas.read_excel(path, sheet_name=None)
+        checking = isinstance(temp_frame, dict)
+        print(temp_frame)
+        if checking == True:
+                print('Is A Dictionary')
+                for key in temp_frame:
+                        individual_dataFrame = (temp_frame[key])
+                        accumulative_frame = pandas.concat([accumulative_frame, individual_dataFrame])
+        else: 
+                print("Is Not a dictionary")
+                accumulative_frame = pandas.concat([accumulative_frame, temp_frame])
+accumulative_frame.to_csv("all_info")
 # for file in glob.iglob(f'{sys.argv[2]}**/*.xlsx', recursive=True):
 #         #Create an instance from the ExcelFile class to check for multiple sheets
 #         temp_frame = pandas.read_excel(file, sheet_name=None)
@@ -19,4 +32,4 @@ for i in glob.iglob('**/*.xlsx', root_dir=dir, recursive=True):
 #                 print(temp_frame)
         #if checking == False:
                 
-#python playground.py all_info_two filesystem/machine1-data/week1
+#python playground.py all_info_two filesystem/machine1-data/
